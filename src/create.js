ce899b93c92113b5cc9c08a256ff80793a130e07
Jin Cheng <jinchengjoker@foxmail.com>
2024-01-10 14:55:28 +0800

    应用生成

--- src/create.js
+++ src/create.js
@@ -2,7 +2,23 @@ import chalk from 'chalk';
 import { $ } from 'execa';
 import prompts from 'prompts';
 
-const codingUrl = 'https://e.coding.net/haplox/hdc/hdc-web.git'
+const templatesConfig = [
+  {
+    key: 'web-app-master',
+    title: 'web 主应用',
+    repoUrl: 'https://e.coding.net/haplox/hapweb/template-web-master.git',
+  },
+  {
+    key: 'web-app',
+    title: 'web 子应用',
+    repoUrl: 'https://e.coding.net/haplox/hapweb/template-web.git',
+  },
+]
+
+const appTypeChoices = templatesConfig.map(t => ({
+  title: t.title,
+  value: t.key
+}))
 
 const init = async () => {
   const response = await prompts([
@@ -10,10 +26,7 @@ const init = async () => {
       type: 'select',
       name: 'appType',
       message: '请选择要创建的应用',
-      choices: [
-        { title: 'web 主应用', value: 'web-app-main' },
-        { title: 'web 子应用', value: 'web-app' },
-      ],
+      choices: appTypeChoices,
     },
     {
       type: 'text',
@@ -21,12 +34,13 @@ const init = async () => {
       message: '请输入应用名称',
     },
   ]);
-  const { appName } = response
+  const { appType, appName } = response
+  const template = templatesConfig.find(t => t.key === appType)
 
   console.log(chalk.gray('🚚 Generating...'))
 
   try {
-    await $`git clone ${codingUrl} ${appName} --depth=1`
+    await $`git clone ${template.repoUrl} ${appName} --depth=1`
     await $`rm -rf ./${appName}/.git`
     console.log(chalk.gray('✅ Generate success!'))
   } catch (error) {
